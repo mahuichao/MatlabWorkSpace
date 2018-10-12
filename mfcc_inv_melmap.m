bc2bb4c0a16bb25ac467c11182ae03f3d81800ad
mahuichao <mahuichao123456@qq.com>
2018-10-12 11:21:31 +0800

    修改仓库结构

--- mfcc_inv_melmap.m
+++
@@ -1,8 +0,0 @@
-function [output] = mfcc_inv_melmap(melFreqs)
-datalength=size(melFreqs,1);
-output=zeros(datalength,1);
-for i=1:datalength
-    output(i,1)=700*(exp(melFreqs(i,1)/1125)-1);
-end
-end
-
--- mfcc_melmap.m
+++
@@ -1,8 +0,0 @@
-function [output] = mfcc_melmap(frequencys)
-    %  ‰»ÎŒ™––
-    output=zeros(size(frequencys,1),1);
-    for i=1:size(frequencys,1)
-        output(i,1)=1125*log(frequencys(i)/700+1);
-    end
-end
-
